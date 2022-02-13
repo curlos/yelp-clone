8294121b6a4cf6f3c77fb8a1bed2cde602f3297b
Carlos Martinez <curlosmart@gmail.com>
2022-02-13 18:32:54 -0500

    get restaurant by id

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const db = require('./db')
 
 const app = express()
 
+// Get all restaurants
 app.get('/api/v1/restaurants', async (req, res) => {
   try {
     const results = await db.query('SELECT * from restaurants;')
@@ -12,6 +13,21 @@ app.get('/api/v1/restaurants', async (req, res) => {
       results: results.rows.length,
       data: {
         restaurants: results.rows
+      }
+    })
+  } catch (err) {
+    console.error(err)
+  }
+})
+
+// Get a restaurant
+app.get('/api/v1/restaurants/:id', async (req, res) => {
+  try {
+    const results = await db.query('select * from restaurants where id = $1', [req.params.id])
+    res.status(200).json({
+      status: "success",
+      data: {
+        restaurant: results.rows[0]
       }
     })
   } catch (err) {
