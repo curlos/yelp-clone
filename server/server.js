df4d80edf24debc1292785061ec04fcc5ebad0c7
Carlos Martinez <curlosmart@gmail.com>
2022-02-14 17:07:00 -0500

    create, update and delete restaurant

--- server/server.js
+++ server/server.js
@@ -1,8 +1,13 @@
 require('dotenv').config()
 const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
 const db = require('./db')
 
 const app = express()
+
+app.use(cors())
+app.use(express.json())
 
 // Get all restaurants
 app.get('/api/v1/restaurants', async (req, res) => {
@@ -29,6 +34,60 @@ app.get('/api/v1/restaurants/:id', async (req, res) => {
       data: {
         restaurant: results.rows[0]
       }
+    })
+  } catch (err) {
+    console.error(err)
+  }
+})
+
+// Create a restaurant
+app.post('/api/v1/restaurants', async (req, res) => {
+  console.log(req.body)
+
+  try {
+    const results = await db.query('INSERT INTO restaurants (name, location, price_range) values ($1, $2, $3)', [req.body.name, req.body.location, req.body.price_range])
+    console.log(results)
+    res.status(200).json({
+      status: "success",
+      data: {
+        restaurant: results
+      }
+    })
+  } catch (err) {
+    console.error(err)
+  }
+})
+
+// Update a restaurant
+app.put('/api/v1/restaurants/:id', async (req, res) => {
+  console.log(req.body)
+
+  try {
+    const results = await db.query(
+      "UPDATE restaurants SET name = $1, location = $2, price_range = $3 where id = $4 returning *",
+      [req.body.name, req.body.location, req.body.price_range, req.params.id]
+    )
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        restaurant: results.rows[0]
+      }
+    })
+  } catch (err) {
+    console.error(err)
+  }
+})
+
+// Delete a restaurant
+app.delete('/api/v1/restaurants/:id', async (req, res) => {
+
+  try {
+    const results = await db.query("DELETE FROM restaurants where id = $1 returning *", [req.params.id])
+    console.log(results)
+
+    res.status(204).json({
+      status: "success",
     })
   } catch (err) {
     console.error(err)
