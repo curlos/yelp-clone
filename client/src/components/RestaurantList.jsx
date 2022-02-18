12dbf392c5ce43f68f8e377bd0c42e7b2158cc21
Carlos Martinez <curlosmart@gmail.com>
2022-02-18 07:36:11 -0500

    show restaurants

--- client/src/components/RestaurantList.jsx
+++ client/src/components/RestaurantList.jsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useContext } from 'react'
-import RestaurantContext from '../context/RestaurantsContext'
+import { RestaurantsContext } from '../context/RestaurantsContext'
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
+import StarRating from './StarRating'
 
 const RestaurantList = () => {
-  const { restaurants, setRestaurants } = useContext(RestaurantContext)
+  const { restaurants, setRestaurants } = useContext(RestaurantsContext)
   const navigate = useNavigate()
 
   useEffect(() => {
@@ -12,7 +13,7 @@ const RestaurantList = () => {
       try {
         const response = await axios.get('http://localhost:3001/api/v1/restaurants')
         console.log(response.data.data)
-        setRestaurants(response.data.data)
+        setRestaurants(response.data.data.restaurants)
       } catch (err) {
 
       }
@@ -59,9 +60,78 @@ const RestaurantList = () => {
     )
   }
 
-  return (
-    <div>
+  console.log(restaurants)
 
+  return (
+    <div className="list-group">
+      <table className="table table-hover">
+        <thead>
+          <tr className="bg-primary text-white">
+            <th scope="col">Restaurant</th>
+            <th scope="col">Location</th>
+            <th scope="col">Price Range</th>
+            <th scope="col">Ratings</th>
+            <th scope="col">Edit</th>
+            <th scope="col">Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {restaurants &&
+            restaurants.map((restaurant) => {
+              return (
+                <tr
+                  onClick={() => handleRestaurantSelect(restaurant.id)}
+                  key={restaurant.id}
+                >
+                  <td>{restaurant.name}</td>
+                  <td>{restaurant.location}</td>
+                  <td>{"$".repeat(restaurant.price_range)}</td>
+                  <td>{renderRating(restaurant)}</td>
+                  <td>
+                    <button
+                      onClick={(e) => handleUpdate(e, restaurant.id)}
+                      className="btn btn-warning"
+                    >
+                      Update
+                    </button>
+                  </td>
+                  <td>
+                    <button
+                      onClick={(e) => handleDelete(e, restaurant.id)}
+                      className="btn btn-danger"
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+          {/* <tr>
+          <td>mcdonalds</td>
+          <td>New YOrk</td>
+          <td>$$</td>
+          <td>Rating</td>
+          <td>
+            <button className="btn btn-warning">Update</button>
+          </td>
+          <td>
+            <button className="btn btn-danger">Delete</button>
+          </td>
+        </tr>
+        <tr>
+          <td>mcdonalds</td>
+          <td>New YOrk</td>
+          <td>$$</td>
+          <td>Rating</td>
+          <td>
+            <button className="btn btn-warning">Update</button>
+          </td>
+          <td>
+            <button className="btn btn-danger">Delete</button>
+          </td>
+        </tr> */}
+        </tbody>
+      </table>
     </div>
   )
 }
